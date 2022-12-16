656d60af04b50177a48460c4d59c711ed4cc9266
Keita Wilson <dkwilson1991@gmail.com>
2022-12-16 16:29:38 +0900

    made dummy data file

---
+++ src/dummyData.js
@@ -0,0 +1,59 @@
+import { v4 as uuidv4 } from "uuid";
+
+const dummyData = [{
+  id: uuidv4(),
+  title: "Todo",
+  tasks: [
+    {
+      id: uuidv4(),
+      title: "Studying React"
+    },
+    {
+      id: uuidv4(),
+      title: "Go study at a cafe"
+    },
+    {
+      id: uuidv4(),
+      title: "Send resume"
+    }
+  ]
+},
+{
+  id: uuidv4(),
+  title: "Todo",
+  tasks: [
+    {
+      id: uuidv4(),
+      title: "Studying Javascript"
+    },
+    {
+      id: uuidv4(),
+      title: "Go study at a cafe"
+    },
+    {
+      id: uuidv4(),
+      title: "Send resume"
+    }
+  ]
+},
+{
+  id: uuidv4(),
+  title: "Todo",
+  tasks: [
+    {
+      id: uuidv4(),
+      title: "Studying CSS"
+    },
+    {
+      id: uuidv4(),
+      title: "Go study at a cafe"
+    },
+    {
+      id: uuidv4(),
+      title: "Send resume"
+    }
+  ]
+}
+];
+
+export default dummyData;
